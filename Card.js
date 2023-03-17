d73b5d70a40d614c31f5ad4d1ebd2beb228dca11
bisonliao <11228490@qq.com>
2023-03-17 10:33:53 +0800

    x

---
+++ Card.js
@@ -0,0 +1,24 @@
+class  Card {
+
+    constructor(p, v)
+    {
+        this.point = p;
+        this.variety = v;
+    }
+
+    getPoint() {
+        return this.point;
+    }
+
+    setPoint(point) {
+        this.point = point;
+    }
+
+    getVariety() {
+        return this.variety;
+    }
+
+    setVariety(variety) {
+        this.variety = variety;
+    }
+}
